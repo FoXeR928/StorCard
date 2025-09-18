7005f70f6aca69e54184585e9749ccc35742aa9a
FoXeR <57914341+FoXeR928@users.noreply.github.com>
2025-09-18 16:04:31 +0300

    Добавлен ответ для проверки сервера

--- api/api_app.py
+++ api/api_app.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 import os
 from loguru import logger
-
 
 def init_pages():
     try:
@@ -42,9 +42,24 @@ try:
     app = FastAPI(
         title="StorCard",
         description="StorCard is your own server for storing discount cards",
-        version="0.4.2-alfa",
+        version="0.4.3-alfa",
+        summary="Сервер",
     )
 except Exception as err:
     logger.critical(f"Ошибка инициализации приложения: {err}")
     exit()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 init_pages()
+
+@app.get("/status", summary="Проверка сервера")
+async def get_status_api():
+    return {"status":"OK"}
